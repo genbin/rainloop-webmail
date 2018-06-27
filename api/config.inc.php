96e4d91badf0336de7c129548a8bec74b2c4d680
liugenbin <genbin.liu@xyre.com>
2018-06-27 22:29:56 +0800

    删掉冗余文件 修改测试服务器ip

--- api/config.inc.php
+++ api/config.inc.php
@@ -5,7 +5,7 @@ $platformHost = 'http://192.168.3.52:9999/platform-app';
 if ($isProduct) {
   include '/var/www/html/rainloop/index.php';
   $mailboxHost = 'http://10.17.4.10';
-  $platformHost = 'http://192.168.3.52:9999/platform-app';
+  $platformHost = 'http://192.168.3.157:9999/platform-app';
 
 } else {
   include '/var/www/index.php';
